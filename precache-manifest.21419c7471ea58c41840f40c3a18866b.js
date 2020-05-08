d428686660c3644aa592e242798f6cc741e01537
jabedek@gmail.com <jabedek@gmail.com>
2020-05-09 00:02:55 +0200

    Updates

---
+++ precache-manifest.21419c7471ea58c41840f40c3a18866b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdcbcdff6f90feb82d178d1177ff9fb8",
+    "url": "/cyberchat/index.html"
+  },
+  {
+    "revision": "b9efa02a60c24c40750c",
+    "url": "/cyberchat/static/css/main.b7fd7154.chunk.css"
+  },
+  {
+    "revision": "07bd57ef5c61b7857b64",
+    "url": "/cyberchat/static/js/2.bdb7dfe4.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/cyberchat/static/js/2.bdb7dfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9efa02a60c24c40750c",
+    "url": "/cyberchat/static/js/main.75cdf15e.chunk.js"
+  },
+  {
+    "revision": "d4b7703ffad9f8328069",
+    "url": "/cyberchat/static/js/runtime-main.58f79dd0.js"
+  },
+  {
+    "revision": "fee7f301bf62f968763e3716167440d0",
+    "url": "/cyberchat/static/media/panorama.fee7f301.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea5b3119ed79dd85378cda6577b9885b.js"
+  "/cyberchat/precache-manifest.21419c7471ea58c41840f40c3a18866b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cyberchat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
