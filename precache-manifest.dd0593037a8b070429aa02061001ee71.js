b860be39fc9dfe592fb113d0b7600ccc4e2d0405
jabedek@gmail.com <jabedek@gmail.com>
2020-05-08 22:38:11 +0200

    Updates

---
+++ precache-manifest.dd0593037a8b070429aa02061001ee71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5330a5af76dc1d223d9c5a574f819fb",
+    "url": "/cyberchat/index.html"
+  },
+  {
+    "revision": "850628edf19a3f5a3d27",
+    "url": "/cyberchat/static/css/main.d0a20423.chunk.css"
+  },
+  {
+    "revision": "07bd57ef5c61b7857b64",
+    "url": "/cyberchat/static/js/2.bdb7dfe4.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/cyberchat/static/js/2.bdb7dfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850628edf19a3f5a3d27",
+    "url": "/cyberchat/static/js/main.124dd0d2.chunk.js"
+  },
+  {
+    "revision": "d4b7703ffad9f8328069",
+    "url": "/cyberchat/static/js/runtime-main.58f79dd0.js"
+  },
+  {
+    "revision": "fee7f301bf62f968763e3716167440d0",
+    "url": "/cyberchat/static/media/panorama.fee7f301.png"
+  }
+]);
